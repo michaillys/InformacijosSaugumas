5ae1f8d1640f640bf5711434913f236362290e71
Michail Lysenko <michaillysenko@gmail.com>
2024-02-29 15:44:09 +0200

    Add project files.

---
+++ InformacijosSaugumas/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InformacijosSaugumas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string text = TextBoxInput.Text;
+            string key = TextBoxKeyInput.Text;                        
+            string encryptedText = VigenaryMethod.Encrypt(text, key);
+            TextBoxOutput.Text = encryptedText;
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {            
+            string text = TextBoxOutput.Text;
+            string key = TextBoxKeyInput.Text;
+            string decryptedText = VigenaryMethod.Decrypt(text, key);
+            TextBoxInput.Text = decryptedText;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string text = TextBoxInput.Text;
+            string key = TextBoxKeyInput.Text;
+            string encryptedText = VigenaryANSIIMethod.Encrypt(text, key);
+            TextBoxOutput.Text = encryptedText;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            string text = TextBoxOutput.Text;
+            string key = TextBoxKeyInput.Text;
+            string decryptedText = VigenaryANSIIMethod.Decrypt(text, key);
+            TextBoxInput.Text = decryptedText;
+        }
+
+        private void TextBoxKeyInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBoxOutput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+        private void TextBoxInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ InformacijosSaugumas/Vigenere.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InformacijosSaugumas
+{
+    public class VigenaryMethod
+    {
+        private static char[] characters = "abcdefghijklmnopqrstuvwxyz0123456789".ToCharArray();
+        public static string Encrypt(string text, string key)
+        {            
+            string newKey = "";
+            int j = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    newKey += " ";
+                }
+                else
+                {
+                    newKey += key[j];
+                    j++;
+                    if (j == key.Length)
+                    {
+                        j = 0;
+                    }
+                }
+            }
+
+            StringBuilder encryptedText = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    encryptedText.Append(" ");
+                }
+                else
+                {                    
+                    text = text.ToLower();
+
+                    int characterPosition = Array.IndexOf(characters, text[i]);
+
+                    int keyPosition = Array.IndexOf(characters, newKey[i]);
+
+                    int encryptedIndex = (characterPosition + keyPosition) % characters.Length;
+                                        
+                    encryptedText.Append(characters[encryptedIndex]);
+                }
+            }
+
+            return encryptedText.ToString();
+        }
+
+        public static string Decrypt(string text, string key)
+        {           
+            string newKey = "";
+            int j = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    newKey += " ";
+                }
+                else
+                {
+                    newKey += key[j];
+                    j++;
+                    if (j == key.Length)
+                    {
+                        j = 0;
+                    }
+                }
+            }
+
+            StringBuilder decryptedText = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    decryptedText.Append(" ");
+                }
+                else
+                {
+                    text = text.ToLower();
+
+                    int characterPosition = Array.IndexOf(characters, text[i]);
+
+                    int keyPosition = Array.IndexOf(characters, newKey[i]);
+
+                    int decryptedIndex = (characterPosition - keyPosition + characters.Length) % characters.Length;
+
+                    decryptedText.Append(characters[decryptedIndex]);                   
+                                      
+                }
+            }
+
+            return decryptedText.ToString();
+        }
+    }
+
+    public class VigenaryANSIIMethod
+    {
+        
+        public static string Encrypt(string text, string key)
+        {
+            string newKey = "";
+            int j = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    newKey += " ";
+                }
+                else
+                {
+                    newKey += key[j];
+                    j++;
+                    if (j == key.Length)
+                    {
+                        j = 0;
+                    }
+                }
+            }
+
+            StringBuilder encryptedText = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    encryptedText.Append(" ");
+                }
+                else
+                {
+                    int characterPosition = (int)text[i] - 32;
+                    int keyPosition = (int)newKey[i] - 32;
+                    int encryptedIndex = (characterPosition + keyPosition) % 95;                    
+                    encryptedText.Append((char)(encryptedIndex + 32));
+                }
+            }
+
+            return encryptedText.ToString();
+        }
+
+        public static string Decrypt(string text, string key)
+        {
+            string newKey = "";
+            int j = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    newKey += " ";
+                }
+                else
+                {
+                    newKey += key[j];
+                    j++;
+                    if (j == key.Length)
+                    {
+                        j = 0;
+                    }
+                }
+            }
+
+            StringBuilder decryptedText = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    decryptedText.Append(" ");
+                }
+                else
+                {
+                    int characterPosition = (int)text[i] - 32;
+                    int keyPosition = (int)newKey[i] - 32;
+                    int decryptedIndex = (characterPosition - keyPosition + 95) % 95;
+                    decryptedText.Append((char)(decryptedIndex + 32));
+                }
+            }
+
+            return decryptedText.ToString();
+        }
+    }
+}
